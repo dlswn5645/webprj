cd9572e6cae2effd43a7657c9d90f216dec6f8da
Stephen <dlswn5645@naver.com>
2021-04-05 10:40:56 +0900

    common commit

---
+++ src/main/java/com/myapp/webpri/WebpriApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.webpri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/webpri/WebpriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.webpri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
