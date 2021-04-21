0a0d19eda209319ed9d2e1769da2d03f5b5574e3
Stephen <dlswn5645@naver.com>
2021-04-21 10:52:58 +0900

    [21.04.21] #파일 업로드 학습 완료

---
+++ src/test/java/com/myapp/webprj/util/FileUtilsTest.java
@@ -0,0 +1,23 @@
+package com.myapp.webprj.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilsTest {
+    
+    @Test
+    void uuidTest(){
+        String fileName = "dog.jpg";
+        String newFileName = UUID.randomUUID().toString() + "_" + fileName;
+        System.out.println("newFileName = " + newFileName);
+    }
+    @Test
+    void makeNewUploadPathTest() {
+        String newUploadPath = FileUtils.getNewUploadPath("D:\\developing_sij\\upload");
+        System.out.println("newUploadPath = " + newUploadPath);
+    }
+
+}
